03da272cc18377c8f26093e71bc0137161298313
Git Chat Interface <gci@example.com>
2024-02-05 13:37:26 -0700

    Update endpoint to return 'Goodbye World'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ instance = Flask(__name__)
 # Add a single endpoint
 @instance.route("/")
 def hello_fnr():
-    return "Hello World"
+    return "Goodbye World"
 
 if __name__ == '__main__':
     instance.run()
